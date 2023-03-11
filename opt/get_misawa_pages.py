17a5084734048faf3e1fdd66450a66fb3442545b
Moe Irikida <irimodi@gmail.com>
2023-03-11 22:26:57 +0900

    file download

--- opt/get_misawa_pages.py
+++ opt/get_misawa_pages.py
@@ -10,3 +10,9 @@ html = response.text
 soup = BeautifulSoup(html, 'html.parser')
 title = soup.title.string
 print(title)
+with open("test.txt", "w") as f:
+    f.write(title)
+# search_form = soup.find('form', {'class': 'tsf'})
+# input_tag = search_form.find('input', {'name': 'q'})
+# input_name = input_tag['name']
+# print(input_name)
