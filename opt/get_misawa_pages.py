98eb55639ff3aae52daa73f1034d825b0787a491
Moe Irikida <irimodi@gmail.com>
2023-03-11 22:48:26 +0900

    欲しい値取れた
    https://qiita.com/Chanmoro/items/db51658b073acddea4ac

--- opt/get_misawa_pages.py
+++ opt/get_misawa_pages.py
@@ -6,12 +6,23 @@ from bs4 import BeautifulSoup
 url = "http://blog.livedoor.jp/jigokuno_misawa/eid_69773305.html"
 response = requests.get(url)
 html = response.text
-
-# div.article-body-inner->div->img src
-# div.article-body-inner->div->img alt
-# div.article-category->a->text
-
 soup = BeautifulSoup(html, 'html.parser')
+
+# div.article-body-inner->div->img src 画像
+body_inner = soup.find('div', {'class': 'article-body-inner'})
+img = body_inner.find('img').get('src')
+print(img)
+# div.article-body-inner->div->img alt 画像内文字
+alt = body_inner.find('img').get('alt')
+print(alt)
+# div.article-category->a->text キャラ名
+category = soup.find('div', {'class': 'article-category'})
+characeter = category.find('a').get_text()
+print(characeter)
+
+# with open("test.txt", "w") as f:
+#     f.write(html)
+
 next_page = soup.find('div', {'class': 'nextPage'})
 link = next_page.find('a').get('href')
 print(link)
